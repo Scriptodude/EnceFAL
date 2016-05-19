447ad0bb746a1d3fd9ad4c894b71819f83ad20f6
scriptodude <scriptodude@gmail.com>
2016-05-19 17:47:42 -0400

    Removed the further "round money" method

--- project/encefal/forms.py
+++ project/encefal/forms.py
@@ -5,28 +5,31 @@ from project.encefal.models import Exemplaire,Vendeur,Livre,ETAT_LIVRE_CHOICES
 
 class IsbnTextInput(TextInput):
 
-    def __init__(self, attrs=None):
-        if attrs:
-            attrs['onchange'] = 'get_isbn(event);'
-        else:
-            attrs = {'onchange':'get_isbn(event);'}
+	def __init__(self, attrs=None):
+		if attrs:
+			attrs['onchange'] = 'get_isbn(event);'
+		else:
+			attrs = {'onchange':'get_isbn(event);'}
 
-        super(TextInput, self).__init__(attrs) 
+		super(TextInput, self).__init__(attrs) 
+		
 
 class IdentifiantTextInput(TextInput):
 
-    def __init__(self, attrs=None):
-        if attrs:
-            attrs['onchange'] = 'get_exemplaire(event);'
-        else:
-            attrs = {'onchange':'get_exemplaire(event);'}
+	def __init__(self, attrs=None):
+		if attrs:
+			attrs['onchange'] = 'get_exemplaire(event);'
+		else:
+			attrs = {'onchange':'get_exemplaire(event);'}
 
-        super(TextInput, self).__init__(attrs) 
+		super(TextInput, self).__init__(attrs) 
+        
 
 class ExemplaireReceptionForm(ModelForm):
-    exclude = ( 'actif', 'livre', 'etat',)
+	#exclude = ( 'actif', 'livre', 'etat' )
 
-    isbn = CharField(
+
+	isbn = CharField(
                      required=True, 
                      help_text="Scannez le code barre du livre",
                      label="ISBN",
@@ -34,63 +37,65 @@ class ExemplaireReceptionForm(ModelForm):
                      max_length=13
                     )
 
-    titre = CharField(required=True, 
+	titre = CharField(required=True, 
                       label="Titre",
                       help_text="Titre")
 
-    auteur = CharField(required=True, 
+	auteur = CharField(required=True, 
                        label="Auteur",
                        help_text="Auteur")
     
-    def clean(self):
-        cleaned_data = super(ExemplaireReceptionForm, self).clean()
+	def clean(self):
+		cleaned_data = super(ExemplaireReceptionForm, self).clean()
         
-        livre, created = Livre.objects.get_or_create(isbn=cleaned_data.get('isbn'))
-        if created:
-            livre.auteur = cleaned_data.get('auteur')
-            livre.titre = cleaned_data.get('titre')
+		livre, created = Livre.objects.get_or_create(isbn=cleaned_data.get('isbn'))
+		if created:
+			livre.auteur = cleaned_data.get('auteur')
+			livre.titre = cleaned_data.get('titre')
 
-        livre.save()
-        self.instance.livre = livre
+		livre.save()
+		self.instance.livre = livre
         
-        return cleaned_data
+		return cleaned_data
 
-    class Meta:
-        model = Exemplaire
+	class Meta:
+		model = Exemplaire
+		exclude = ( 'actif', 'livre', 'etat' )
+		
 
 class ExemplaireVenteForm(ModelForm):
+	#exclude = ( 'actif', 'livre', 'etat' )
+	
+	isbn = CharField(required=True,
+					help_text="Scannez le code barre du livre",
+					label="ISBN",
+					max_length=13)
 
-    exclude = ( 'actif', 'livre', 'etat',)
+	titre = CharField(	required=True, 
+                      	label="Titre",
+                      	help_text="Titre")
 
-    isbn = CharField(required=True, 
-                        help_text="Scannez le code barre du livre",
-                        label="ISBN",
-                        max_length=13)
+	identifiant = IntegerField(	required=True, 
+                      	label="Identifiant",
+                      	help_text="Identifiant",
+                      	widget=IdentifiantTextInput)
 
-    titre = CharField(required=True, 
-                      label="Titre",
-                      help_text="Titre")
-
-    identifiant = IntegerField(required=True, 
-                      label="Identifiant",
-                      help_text="Identifiant",
-                      widget=IdentifiantTextInput)
-
-    auteur = CharField(required=True, 
-                       label="Auteur",
-                       help_text="Auteur")
+	auteur = CharField(	required=True, 
+                       	label="Auteur",
+                       	help_text="Auteur")
     
-    prix = DecimalField(required=True, 
-                        label="Prix demandé",
-                        help_text="Prix")
+	prix = DecimalField(	required=True, 
+                        	label="Prix demandé",
+                        	help_text="Prix")
 
-    def clean(self):
-        cleaned_data = super(ExemplaireVenteForm, self).clean()
-        self.instance = Exemplaire.objects.get(pk=cleaned_data['identifiant'])
-        self.instance.etat = 'VEND'
-        self.instance.save()
-        return cleaned_data
+	def clean(self):
+		cleaned_data = super(ExemplaireVenteForm, self).clean()
+		self.instance = Exemplaire.objects.get(pk=cleaned_data['identifiant'])
+		self.instance.etat = 'VEND'
+		self.instance.save()
+		return cleaned_data
 
-    class Meta:
-        model = Exemplaire
+	class Meta:
+		model = Exemplaire
+		exclude = ( 'actif', 'livre', 'etat' )
 
