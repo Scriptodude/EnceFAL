0fae7f5d3c307fc32fb1d3b9e1f392dfbb1c5363
Jonathan Lavigne <johny.boy80@yahoo.ca>
2016-05-24 10:09:19 -0400

    Fixed Random auto-indent

--- project/encefal/models.py
+++ project/encefal/models.py
@@ -217,8 +217,8 @@ class Exemplaire(Metadata):
 	etat = models.CharField(max_length=4, choices=ETAT_LIVRE_CHOICES,
                             default='VENT', verbose_name='État', )
 	prix = models.DecimalField(default=0.00, max_digits=5,
-								decimal_places=2,
-								help_text='Doit être un prix valide entre 0.00 et 999.99')
+				decimal_places=2,
+				help_text='Doit être un prix valide entre 0.00 et 999.99')
 
 	def __unicode__(self):
 		return self.livre.__unicode__()
