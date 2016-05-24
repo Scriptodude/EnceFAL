b47377e439672ac105c551367cbe7ecfa622fc3c
Jonathan Lavigne <johny.boy80@yahoo.ca>
2016-05-24 12:18:40 -0400

    Update tests.py

--- project/encefal/tests.py
+++ project/encefal/tests.py
@@ -20,7 +20,8 @@ class SimpleTest(TestCase):
 	# Creation des objets de test
 	def setUp(self):
 		print("Creating objects...")
-		Vendeur.objects.create(nom="Robert", prenom="Bob",
+		Vendeur.objects.create(nom="Robert",
+				prenom="Bob",
 				code_permanent="ROBB11223300",
 				email="bob.robert@truc.org")
 		vend = Vendeur.objects.get(nom="Robert")
