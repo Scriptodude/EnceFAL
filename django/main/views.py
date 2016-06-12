aee1823289f9c7ac5e6515762d658ed819228779
scriptodude <scriptodude@gmail.com>
2016-06-11 20:30:08 -0400

    Added view for vendeurs

--- django/main/views.py
+++ django/main/views.py
@@ -3,7 +3,7 @@ import json
 import urllib
 from lxml import html
 
-from datetime import datetime, timedelta, date
+import datetime
 from django.conf import settings
 from django.shortcuts import render_to_response,render
 from django.template import RequestContext, Context
@@ -51,58 +51,58 @@ def livre(request):
     if not request.user.is_authenticated():
         return HttpResponseNotFound()
 
-	assert('isbn' in request.GET)
-	assert('nb' in request.GET)
-	assert(len(request.GET['isbn']) in [10,13])
+    assert('isbn' in request.GET)
+    assert('nb' in request.GET)
+    assert(len(request.GET['isbn']) in [10,13])
 
-	reponse = None
-	livre = None
-	isbn = request.GET['isbn']
-	nb = request.GET['nb']
+    reponse = None
+    livre = None
+    isbn = request.GET['isbn']
+    nb = request.GET['nb']
 
-	try:
-		livre = Livre.objects.get(isbn=isbn)
-	except Livre.DoesNotExist:
-		pass
-	
-	if not livre:
-		query = ISBN_DB_BASE_QUERY.format(settings.ISBNDB_API_KEY, isbn)
-		reponse_query = json.load(urllib.urlopen(query))
-		if 'error' not in reponse_query:
-			reponse_query = reponse_query['data'][0]
+    try:
+	    livre = Livre.objects.get(isbn=isbn)
+    except Livre.DoesNotExist:
+	    pass
 
-			titre = reponse_query['title'] if reponse_query['title'] else ''
-			auteur = reponse_query['author_data'][0]['name'] if reponse_query['author_data'] else ''
+    if not livre:
+	    query = ISBN_DB_BASE_QUERY.format(settings.ISBNDB_API_KEY, isbn)
+	    reponse_query = json.load(urllib.urlopen(query))
+	    if 'error' not in reponse_query:
+		    reponse_query = reponse_query['data'][0]
 
-			reponse = {'titre':titre,
-					'auteur':auteur,
-					'nb':nb}
-		else:
+		    titre = reponse_query['title'] if reponse_query['title'] else ''
+		    auteur = reponse_query['author_data'][0]['name'] if reponse_query['author_data'] else ''
 
-			query = COOP_UQAM_BASE_QUERY.format(isbn)
-			reponse_query = urllib.urlopen(query)
-			tree = html.fromstring(reponse_query.read())
+		    reponse = {'titre':titre,
+				    'auteur':auteur,
+				    'nb':nb}
+	    else:
 
-			titres = tree.cssselect("h3 a")
-			if titres:
+		    query = COOP_UQAM_BASE_QUERY.format(isbn)
+		    reponse_query = urllib.urlopen(query)
+		    tree = html.fromstring(reponse_query.read())
 
-				titre = titres[0].text
+		    titres = tree.cssselect("h3 a")
+		    if titres:
 
-				auteur = tree.cssselect("h3 + p")[0].text_content().split(' : ')[1].split('\r')[0].strip()
+			    titre = titres[0].text
 
-				reponse = {'titre':titre,
+			    auteur = tree.cssselect("h3 + p")[0].text_content().split(' : ')[1].split('\r')[0].strip()
+
+			    reponse = {'titre':titre,
                            'auteur':auteur,
                            'nb':nb}
 
-	else:
-		reponse = {'titre':livre.titre,
-				'auteur':livre.auteur,
+    else:
+	    reponse = {'titre':livre.titre,
+			    'auteur':livre.auteur,
                    'nb':nb}
 
-	if reponse:
-		return HttpResponse(json.dumps(reponse), content_type="application/json")
-	else:
-		return HttpResponseNotFound()
+    if reponse:
+	    return HttpResponse(json.dumps(reponse), content_type="application/json")
+    else:
+	    return HttpResponseNotFound()
 
 def exemplaire(request):
 
@@ -120,9 +120,7 @@ def exemplaire(request):
     except Exemplaire.DoesNotExist:
         return HttpResponseNotFound()
 
-	print("second Asserts")
 	assert(exemplaire.livre)
-	print("Second asserts done")
 
     if exemplaire.etat != 'VENT':
         reponse = {
@@ -175,9 +173,15 @@ def factures(request):
 		id_facture = request.GET['id']
 
 		try:
-			facture = Facture.objects.get(id=id_facture)
-		except Facture.DoesNotExist:
-		    facture = None
+			id_facture = int(id_facture)
+
+			try:
+				facture = Facture.objects.get(id=id_facture)
+			except Facture.DoesNotExist:
+				facture = None
+		except:
+			facture = None
+
 	else:
 		# Hack louche pour l'instant
 		facture = 0
@@ -188,4 +192,29 @@ def factures(request):
 	}
 
 	return render(request, 'encefal/factures.html', context)
+
+def livresVendeur(request):
+	context={}
+
+	if 'code_perm' in request.GET and request.GET['code_perm']:
+		code_vend = request.GET['code_perm']
+
+		# Afficher recu
+		try:
+		    vendeur = Vendeur.objects.get(code_permanent=code_vend)
+		    context['vendeur'] = vendeur
+		    context['exemplaires'] = vendeur.exemplaires.all()
+
+		except Vendeur.DoesNotExist:
+		    context['vendeur'] = None
+		    context['exemplaires'] = []
+
+		context['date_transaction'] = datetime.date.today()
+		context['employe'] = 'Non-disponible'
+		context['montant_total'] = sum([e.prix for e in context['exemplaires']]) or 0
+
+	context['from_view']=True
+	context['ext']='base.html'
+
+	return render(request, 'encefal/depots.html', context)
 
