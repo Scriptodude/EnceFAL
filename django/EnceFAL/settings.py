ff75049d97b93e773ce338666565c7e2c1555e51
scriptodude <scriptodude@gmail.com>
2016-06-14 10:20:25 -0400

    Resolving Conflicts

--- django/EnceFAL/settings.py
+++ django/EnceFAL/settings.py
@@ -119,8 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-ALLOWED_HOSTS = [ "localhost" ]
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
--- django/main/urls.py
+++ django/main/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     url(r'^exemplaire/$', exemplaire, name='exemplaire'),
     url(r'^vendeur/$', vendeur, name='vendeur'),
     url(r'^factures/$', factures, name='factures'),
-	url(r'^livre_vendeur/$', livresVendeur, name='livres_vendeur'),
 ]
